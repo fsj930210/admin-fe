fc856f7ca202436ebcfb9ea3718d9261ddabb3d2
fsj930210 <fsj930210@163.com>
2023-05-27 20:03:54 +0800

    feat(init): init project

---
+++ react-admin/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import UnoCSS from 'unocss/vite';
+import React from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [UnoCSS(), React()],
+});
